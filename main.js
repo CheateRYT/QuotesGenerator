50ce0e42c9bc4fe80be0ee634c452df48fb0f079
CheateRYT <cheaterytyt@gmail.com>
2023-06-06 22:17:20 +0300

    Update

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-   // Массив с цитатами саморазвития
-   const quotes = [
+
+    // Массив с цитатами саморазвития
+    const quotes = [
     {
       quote: "Саморазвитие - это ключ к достижению своих целей.",
       author: "Аноним"
@@ -13,39 +14,105 @@
       author: "Джим Рон"
     },
     {
-      quote: "Не ограничивайте свои цели - ограничивайте только свое время.",
-      author: "Майкл Фаррис"
+      quote: "Истина не родилась для нас, мы должны родиться для нее.",
+      author: "Ф.М. Достоевский"
     },
     {
-      quote: "Саморазвитие начинается с преодоления комфортной зоны.",
-      author: "Ник Вуйчич"
+      quote: "Самая лучшая инвестиция, это инвестиция в себя.",
+      author: "Уоррен Баффет"
     },
     {
-      quote: "Нет предела для роста и самосовершенствования человека.",
-      author: "Луций Анней Сенека"
+      quote: "Не существует пути к успеху. Успех - это сам путь.",
+      author: "Конфуций"
     },
     {
-      quote: "Чтение - это дорога к знаниям, а знания - это ключ к успеху.",
-      author: "Наполеон Хилл"
+      quote: "Развивайтесь каждый день, даже если это всего лишь небольшой шаг.",
+      author: "Нил Гейман"
+    },
+    {
+      quote: "Успех приходит к тем, кто продолжает учиться после того, как они закончили школу.",
+      author: "Рамит Сети"
+    },
+    {
+      quote: "Образование - это самая мощная валюта, которую вы можете иметь.",
+      author: "Мишель Обама"
+    },
+    {
+      quote: "Истина в том, что вы становитесь тем, кем вы занимаетесь.",
+      author: "Джим Рон"
+    },
+    {
+      quote: "Постоянное саморазвитие - это ключ к преодолению себя и достижению новых вершин.",
+      author: "Брайан Трейси"
+    },
+    {
+      quote: "Инвестируйте в себя. Ваше знание и навыки - ваши наиболее выгодные активы.",
+      author: "Пол Б. Фаррелл"
     },
   ];
 
-  // Функция для получения случайной цитаты из массива
-  function getRandomQuote() {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    const randomQuote = quotes[randomIndex];
+    let currentQuoteIndex = -1; // Текущий индекс цитаты
 
-    const quoteContainer = document.getElementById('quoteContainer');
+    // Функция для получения следующей цитаты
+    function getNextQuote() {
+      currentQuoteIndex++;
+      if (currentQuoteIndex === quotes.length) {
+        currentQuoteIndex = 0;
+      }
 
-    const quoteElement = document.createElement('div');
-    quoteElement.classList.add('quote');
-    quoteElement.innerHTML = `
-      <p>"${randomQuote.quote}"</p>
-      <p>- ${randomQuote.author}</p>
-    `;
+      const quoteContainer = document.getElementById('quoteContainer');
+      quoteContainer.innerHTML = '';
 
-    quoteContainer.appendChild(quoteElement);
-  }
+      const quoteElement = document.createElement('div');
+      quoteElement.classList.add('quote');
+      quoteElement.innerHTML = `
+        <p>"${quotes[currentQuoteIndex].quote}"</p>
+        <p>- ${quotes[currentQuoteIndex].author}</p>
+      `;
 
-  // Запускаем получение случайной цитаты при загрузке страницы
-  getRandomQuote();
+      quoteContainer.appendChild(quoteElement);
+    }
+
+    // Обработчик нажатия на кнопку "Следующая цитата"
+    document.getElementById('nextButton').addEventListener('click', getNextQuote);
+
+    // Загрузка сохраненной цитаты из local storage
+    function loadSavedQuote() {
+      const savedQuote = localStorage.getItem('customQuote');
+      if (savedQuote) {
+        const quoteContainer = document.getElementById('quoteContainer');
+        quoteContainer.innerHTML = '';
+
+        const quoteElement = document.createElement('div');
+        quoteElement.classList.add('quote');
+        quoteElement.innerHTML = `
+          <p>"${savedQuote}"</p>
+          <p>- Вы</p>
+        `;
+
+        quoteContainer.appendChild(quoteElement);
+
+        // Добавление сохраненной цитаты в массив цитат
+        quotes.push({
+          quote: savedQuote,
+          author: 'Вы'
+        });
+      }
+    }
+
+    // Обработчик нажатия на кнопку "Сохранить цитату"
+    document.getElementById('saveButton').addEventListener('click', function() {
+      const customQuoteInput = document.getElementById('customQuoteInput');
+      const customQuote = customQuoteInput.value.trim();
+
+      if (customQuote !== '') {
+        localStorage.setItem('customQuote', customQuote);
+        customQuoteInput.value = '';
+
+        loadSavedQuote();
+      }
+    });
+
+    // Запускаем получение первой цитаты и загрузку сохраненной цитаты при загрузке страницы
+    getNextQuote();
+    loadSavedQuote();
